4f3a4f8e11f16810635c6f86e6f609b41cde76ac
Nikita Pikhtovnikov <144624133+JRPikhtovnikov@users.noreply.github.com>
2026-01-06 15:47:05 +0300

    Исправлены недочеты sync-server

--- sprint1/problems/sync_server/solution/src/main.cpp
+++ sprint1/problems/sync_server/solution/src/main.cpp
@@ -1,10 +1,15 @@
-#include <boost/beast/core.hpp>
-#include <boost/beast/http.hpp> 
-#include <boost/asio/ip/tcp.hpp>
-#include <boost/asio/write.hpp>
+#ifdef WIN32
+#include <sdkddkver.h>
+#endif
+// boost.beast будет использовать std::string_view вместо boost::string_view
+#define BOOST_BEAST_USE_STD_STRING_VIEW
 
-#include <thread>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
 #include <iostream>
+#include <thread>
+#include <optional>
 
 namespace net = boost::asio;
 using tcp = net::ip::tcp;
