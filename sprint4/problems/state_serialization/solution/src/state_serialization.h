8161b7b0f0e4acd02e372742309c0072cf5fff67
Nikita Pikhtovnikov <pikht.nik@gmail.com>
2026-03-12 22:23:35 +0300

    state_serialization solution

---
+++ sprint4/problems/state_serialization/solution/src/state_serialization.h
@@ -0,0 +1,105 @@
+#pragma once
+
+#include "model.h"
+#include "geom.h"
+
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/string.hpp>
+#include <boost/serialization/utility.hpp>
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+namespace serialization {
+
+// Сериализация геометрических типов
+template <typename Archive>
+void serialize(Archive& ar, geom::Point2D& p, [[maybe_unused]] const unsigned int version) {
+    ar & p.x & p.y;
+}
+
+template <typename Archive>
+void serialize(Archive& ar, geom::Vec2D& v, [[maybe_unused]] const unsigned int version) {
+    ar & v.x & v.y;
+}
+
+// Вспомогательная структура для LostObject
+struct LostObjectRepr {
+    uint32_t id = 0;
+    int type = 0;
+    int value = 0;
+    geom::Point2D pos;
+
+    template <typename Archive>
+    void serialize(Archive& ar, [[maybe_unused]] const unsigned int version) {
+        ar & id & type & value & pos;
+    }
+};
+
+// Вспомогательная структура для игрока
+struct PlayerRepr {
+    uint32_t id = 0;
+    std::string name;
+    uint32_t session_id = 0;
+    geom::Point2D pos;
+    geom::Vec2D speed;
+    int dir = 0;                       // model::Direction как int
+    int score = 0;
+    size_t bag_capacity = 0;
+    std::vector<model::BagItem> bag;   // BagItem должен быть сериализуем
+
+    template <typename Archive>
+    void serialize(Archive& ar, [[maybe_unused]] const unsigned int version) {
+        ar & id & name & session_id & pos & speed & dir & score & bag_capacity & bag;
+    }
+};
+
+// Вспомогательная структура для токена
+struct TokenRepr {
+    std::string token;
+    uint32_t player_id = 0;
+
+    template <typename Archive>
+    void serialize(Archive& ar, [[maybe_unused]] const unsigned int version) {
+        ar & token & player_id;
+    }
+};
+
+// Вспомогательная структура для игровой сессии
+struct SessionRepr {
+    uint32_t id = 0;
+    std::string map_id;
+    uint32_t next_loot_id = 0;
+    std::vector<LostObjectRepr> loot_objects;
+
+    template <typename Archive>
+    void serialize(Archive& ar, [[maybe_unused]] const unsigned int version) {
+        ar & id & map_id & next_loot_id & loot_objects;
+    }
+};
+
+// Корневая структура состояния игры
+struct GameStateRepr {
+    uint32_t next_player_id = 0;
+    std::vector<SessionRepr> sessions;
+    std::vector<PlayerRepr> players;
+    std::vector<TokenRepr> tokens;
+
+    template <typename Archive>
+    void serialize(Archive& ar, [[maybe_unused]] const unsigned int version) {
+        ar & next_player_id & sessions & players & tokens;
+    }
+};
+
+} // namespace serialization
+
+// Сериализация BagItem (добавляем прямо в model.h или здесь)
+namespace model {
+    template <typename Archive>
+    void serialize(Archive& ar, BagItem& item, [[maybe_unused]] const unsigned int version) {
+        ar & item.id & item.type & item.value;
+    }
+}
